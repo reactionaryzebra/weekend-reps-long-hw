abee5db7ba3780c22e044d1256945e0f79aada3b
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-22 14:37:10 -0700

    Completed Section 1 part C

--- js/app.js
+++ js/app.js
@@ -20,3 +20,21 @@ secondVariable = 'Any String';
 //the value of firstVariable is 5
 const yourName = 'Justin';
 console.log(`Hello, my name is ${yourName}`);
+
+//Section 1C
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(true != false);
+console.log(false || false || false || false || false || false || true);
+console.log(false === false)
+console.log(e === 'Kevin');
+console.log(a + b === c);
+console.log(a * a === d);
+console.log(48 == '48');
