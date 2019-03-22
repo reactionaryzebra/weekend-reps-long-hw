197729b09170694b106b7b754b67d1eb32db54c6
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-22 14:34:50 -0700

    Completed Section 1 part B

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Section 1
+//Section 1A
 //1) We assign a value to a variable with a '='
 //2) We can change the value of something in many ways.  You can re-assign it with an =, you can manipulate it
 //with operators like +=, -=, or methods like .toString, .map, etc.
@@ -10,3 +10,13 @@
 //often in plain english.  THis allows you to break the problem down into parts and confront the logic of it versus
 //focusing on the syntax of actually coding something.
 //6) Roughly 75% of your time should be focused on planning vs actually typing something out.
+
+//Section 1B
+let firstVariable;
+firstVariable = 'Hello World';
+firstVariable = 5;
+let secondVariable = firstVariable;
+secondVariable = 'Any String';
+//the value of firstVariable is 5
+const yourName = 'Justin';
+console.log(`Hello, my name is ${yourName}`);
