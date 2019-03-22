2640f1e6f7d457df97298e12f86837d6b5c58ed6
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-22 14:29:45 -0700

    Completed section 1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//Section 1
+//1) We assign a value to a variable with a '='
+//2) We can change the value of something in many ways.  You can re-assign it with an =, you can manipulate it
+//with operators like +=, -=, or methods like .toString, .map, etc.
+//3) const newVar = oldVar;
+//4) declaring is essentially creating.  const newVar; declares a variable called newVar without assigning it any value.
+//assigning is when we are giving a value to a variable.  The primary purpose of defining something is to initialize it
+// versus the primary purpose of assigning is to manipulate value.
+//5) pseudocoding is the process of breaking down a coding problem into parts and explaining what you plan on doing
+//often in plain english.  THis allows you to break the problem down into parts and confront the logic of it versus
+//focusing on the syntax of actually coding something.
+//6) Roughly 75% of your time should be focused on planning vs actually typing something out.
