dc9c94f452619e6124f9b64fa28b007d7ae145d4
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-23 14:13:19 -0700

    Completed section 3H

--- js/app.js
+++ js/app.js
@@ -154,3 +154,47 @@ if (number < 5){
 } else {
   console.log('monkey');
 }
+
+//Section 3H
+const kristynsCloset = [
+  'left shoe',
+  'cowboy boots',
+  'right sock',
+  'GA hoodie',
+  'green pants',
+  'yellow knit hat',
+  'marshmallow peeps'
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    'grey button-up',
+    'dark grey button-up',
+    'light blue button-up',
+    'blue button-up'
+  ],
+  [
+    // These are Thom's pants
+    'grey jeans',
+    'jeans',
+    'PJs'
+  ],
+  [
+    // Thom's accessories
+    'wool mittens',
+    'wool scarf',
+    'raybans'
+  ]
+];
+//1
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+//2
+kristynsCloset.splice(6, 0, 'raybans');
+//3
+kristynsCloset[5] = 'stained knit hat';
+//4, 5, 6, 7
+console.log(`Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][2]} and ${thomsCloset[2][2]}!`);
+//8
+thomsCloset[1][2] = 'Footie Pajamas';
